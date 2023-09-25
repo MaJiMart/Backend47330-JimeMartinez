c8ba4275465165c930d031e6dad2ede6eac59985
Jimena Martinez <majimartinez@gmail.com>
2023-09-25 12:55:11 +0200

    challenge class04 finished

--- weeklyChallenges/class04Challenge/class04Challenge.js
+++ weeklyChallenges/class04Challenge/class04Challenge.js
@@ -4,25 +4,142 @@ const path = require("path");
 class ProductsManager {
     constructor(path) {
     this.path = path;
-    }
-
-    async createProduct(product) {
-        const { title, description, price, thumbnail, code, stock } = product;
-        if (!title || !description || !price || !thumbnail || !code || !stock){
-            throw new Error ('Por favor, complete todos los campos.')
-        }
-        const products = await getJSONFromFile(this.path);
-        const id = products.length + 1;
-        const newProduct = { id, title, description, price, thumbnail, code, stock };
-        products.push(newProduct);
-
-        return saveJSONToFile(this.path, products)
-    }
-
-    getProduct() {
-        return getJSONFromFile(this.path)
-    }
 }
+
+async createProduct(product) {
+    const { title, description, price, thumbnail, code, stock } = product;
+    if (!title || !description || !price || !thumbnail || !code || !stock){
+        throw new Error ('Por favor, complete todos los campos.')
+    }
+    const products = await getJSONFromFile(this.path);
+    const id = generateID()    
+    const newProduct = { id, title, description, price, thumbnail, code, stock };
+    products.push(newProduct);
+    
+    return saveJSONToFile(this.path, products)
+}
+
+getProduct() {    
+    return getJSONFromFile(this.path);
+}
+
+async getProductById(id) {
+    const products = await getJSONFromFile(this.path);
+    const product = products.find((product) => product.id === id);
+    if (!product) {
+        throw new Error(`No se encontró un producto con el ID ${id}`);
+    }
+    return product;
+}
+
+async updateProduct(id, updatedProduct) {
+    const products = await getJSONFromFile(this.path);
+    const index = products.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+        throw new Error(`No se encontró el producto con el ID ${id}`);
+    }
+
+    products[index] = updatedProduct;
+
+    await saveJSONToFile(this.path, products);
+}
+
+async deleteProduct(id) {
+    const products = await getJSONFromFile(this.path);
+    const index = products.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+        throw new Error(`Producto con ID ${id} no encontrado`);
+    }
+
+    products.splice(index, 1);
+
+    await saveJSONToFile(this.path, products);
+}
+}
+
+//Crear producto
+(async function(run){
+    
+    if (!run) return;
+    
+    const productsManager = new ProductsManager('./products.json')
+    
+    await productsManager.createProduct({
+        title: 'producto prueba',
+        description: 'Este es otro producto prueba más',
+        price: 500,
+        thumbnail: 'Sin imagen',
+        code: 'xyz123',
+        stock: 5,
+    });
+    
+    const products = await productsManager.getProduct();
+    console.log('🔍 Los productos encontrados son:', products)
+})(false);
+
+//Buscar producto por Id
+(async function(run){
+        
+    if (!run) return;
+    
+    const productsManager = new ProductsManager('./products.json')
+    
+    try {
+        const productId = '1'; // Reemplaza con el ID del producto que deseas buscar
+        const product = await productsManager.getProductById(productId);
+        console.log(`🔍 El producto con id ${productId} que buscas es: `, product);
+    } catch (error) {
+        console.error('😓 SORRY! Product not found');
+    }
+})(false);
+
+//Actualizar producto
+(async function (run) {
+    if (!run) return;
+    
+    const productsManager = new ProductsManager('./products.json')
+    
+    try {
+        const productIdToUpdate = '9'; // Reemplaza con el ID del producto que deseas actualizar
+        const updatedProduct = {
+            id: productIdToUpdate,
+            title: 'Producto actualizado', // reemplazar 
+            description: 'Este es el producto actualizado',// reemplazar
+            price: 750, // reemplazar
+            thumbnail: 'Nueva imagen', // reemplazar
+            code: 'abf756', // reemplazar
+            stock: 100, // reemplazar
+        };
+
+        await productsManager.updateProduct(productIdToUpdate, updatedProduct);
+        console.log(`✅ El producto con ID ${productIdToUpdate} fue actualizado con éxito.`);
+        
+        const products = await productsManager.getProduct();
+        console.log('El producto actualizados es:', updatedProduct);
+    } catch (error) {
+        console.error('😓 Error al actualizar el producto:', error.message);
+    }
+})(false);
+
+//Eliminar producto
+(async function (run) {
+    if (!run) return;
+
+    const productsManager = new ProductsManager('./products.json')
+    
+    try {
+        const productIdToDelete = '9'; // Reemplaza con el ID del producto que deseas eliminar
+        await productsManager.deleteProduct(productIdToDelete);
+        console.log(`✅ El producto con ID ${productIdToDelete} fue eliminado con éxito.`);
+        
+        const products = await productsManager.getProduct();
+        console.log('La lista actualizada de productos es:', products);
+    } catch (error) {
+        console.error('😓 ERROR! No pudimos eliminar el producto:', error.message);
+    }
+})(true);
 
 //Utilities
 const fileExist = async (path) => {
@@ -60,22 +177,12 @@ const saveJSONToFile = async (path, data) => {
     }
 };
 
-(async function(run){
-
-    if (!run) return;
-    
-    const productsManager = new ProductsManager('./products.json')
-    
-    await productsManager.createProduct({
-        title: 'producto prueba 4',
-        description: 'Este es el cuarto producto prueba',
-        price: 500,
-        thumbnail: 'Sin imagen',
-        code: 'xyz123',
-        stock: 5,
-    });
-    
-    const products = await productsManager.getProduct();
-    console.log('🔍 Los productos encontrados son:', products)
-
-})(true);
+function generateID(){
+    let d= new Date().getTime();
+    let id = 'xxxxxxxxxx'.replace(/[xy]/g, function (c){
+        let r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r: (r & 0x3 | 0x8)).toString(16);
+    })
+    return id
+};
